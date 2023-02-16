8c402180aa5ca63dfabeec58c8d22a001e394e80
deneme <spoonnoisthere@gmail.com>
2023-02-16 19:05:03 +0300

    Hello World
    Hello World

---
+++ 001/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void printHelloWorld();
+
+int main(){
+	printf("Hello World\n");
+		
+	
+	// ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ 
+	
+	
+	int i;
+	for(i = 0; i<=7; i++){
+		printf("Hello World\n");
+	}
+	
+	// ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ ¿ 
+	
+	
+	printHelloWorld();
+	
+
+	return 0;
+}
+
+
+void printHelloWorld(){
+	printf("Hello World function");
+}
